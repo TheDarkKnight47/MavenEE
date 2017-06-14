76a2c070877a859b1c206aaf74f31779e9d84d7a
Ajay Sharma <ajay.sharma@finicity.com>
2017-06-14 12:37:19 +0530

    - Second branch

--- MavenStandalone/src/test/java/com/mycompany/mavenstandalone/MainClassTest.java
+++ MavenStandalone/src/test/java/com/mycompany/mavenstandalone/MainClassTest.java
@@ -36,7 +36,7 @@ public class MainClassTest {
         String[] args = null;
         MainClass.main(args);
         // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
+        fail("The test case is a prototype.");
     }
     
 }
